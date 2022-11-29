ff878e5c8699090257351907264d0b61e929d907
Anna Taikh <kozina.aniuta2014@yandex.ru>
2022-11-29 15:00:35 +0300

    feat: adaptive popap + js

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+let openPopup = document.querySelector('.profile__info_edit-button');
+let closePopup = document.querySelector('.popup__close-button');
+let popUp = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__info_name');
+let profileText = document.querySelector('.profile__info_text');
+let popupName = document.querySelector('.popup__name');
+let popupText = document.querySelector('.popup__text');
+let popupSave = document.querySelector('.popup__save-button');
+
+function popupOpened() {
+    popUp.classList.add('popup_opened');
+    popupName.value = profileName.textContent;
+    popupText.value = profileText.textContent;
+}
+
+function popupClosed() {
+    popUp.classList.remove('popup_opened');
+}
+
+function popapSave(e) {
+    e.preventDefault();
+    profileName.textContent = popupName.value;
+    profileText.textContent = popupText.value;
+    popupClosed();
+}
+
+openPopup.addEventListener('click', popupOpened);
+closePopup.addEventListener('click', popupClosed);
+popupSave.addEventListener('click', popapSave);
+
+popUp.addEventListener('click', function (closePopup) {
+    if (closePopup.target === closePopup.currentTarget) {
+        popupClosed();
+    }
+})
