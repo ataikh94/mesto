7883360e2fb2573479e35e12c60738102800b123
Anna Taikh <kozina.aniuta2014@yandex.ru>
2022-12-01 10:36:28 +0300

    fix: исправлены замечания ревьюера (после попытки 2)

--- scripts/index.js
+++ scripts/index.js
@@ -5,34 +5,34 @@ let popUp = document.querySelector('.popup');
 let profileName = document.querySelector('.info__name');
 let profileText = document.querySelector('.info__text');
 let editForm = document.querySelector('.popup__form');
-let popupName = editForm.querySelector('.popup__input_name');
-let popupText = editForm.querySelector('.popup__input_text');
+let popupName = editForm.querySelector('.popup__input_type_name');
+let popupText = editForm.querySelector('.popup__input_type_text');
 
 
 /* Функция открытия модального окна */
-function popupOpened() {
+function openedPopup() {
     popUp.classList.add('popup_opened');
     popupName.value = profileName.textContent;
     popupText.value = profileText.textContent;
 }
 
 /* Функция закрытия модального окна */
-function popupClosed() {
+function closedPopup() {
     popUp.classList.remove('popup_opened');
 }
 
 /* Функция сохранения информации */
-function popapSave(e) {
+function savedPopup(e) {
     e.preventDefault();
     profileName.textContent = popupName.value;
     profileText.textContent = popupText.value;
-    popupClosed();
+    closedPopup();
 }
 
 /* События */
-openPopup.addEventListener('click', popupOpened);
-closePopup.addEventListener('click', popupClosed);
-editForm.addEventListener('submit', popapSave);
+openPopup.addEventListener('click', openedPopup);
+closePopup.addEventListener('click', closedPopup);
+editForm.addEventListener('submit', savedPopup);
 
 /* popUp.addEventListener('click', function (closePopup) {
     if (closePopup.target === closePopup.currentTarget) {
